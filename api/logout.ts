1db9899abb01d4af8d0bc32be488cb97448d9c59
mtmnikki <nikki@mtmtft.com>
2025-08-16 23:55:44 -0500

    Delete api directory

--- api/logout.ts
+++
@@ -1,7 +0,0 @@
-// /api/logout.ts
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(_: VercelRequest, res: VercelResponse) {
-  res.setHeader('Set-Cookie', 'crxq_token=; Path=/; HttpOnly; Max-Age=0; SameSite=Lax; Secure');
-  res.status(200).json({ ok: true });
-}
